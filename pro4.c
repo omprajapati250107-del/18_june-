07c2fd25f5c5c2a82bba1f644135e1a376d47528
omprajapati_08 <omprajapati250107@gmail.com>
2026-07-01 23:17:12 +0530

    just added

---
+++ pro4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    // step 1 : variable declaration 
+    int age;   // here, int is a data type and age is a variablename 
+    
+    // step 2 : variable initilization 
+    age = 20;  // here age is a variable name and 20 is value 
+    
+    printf("Hello, My age is %d",age);
+}
