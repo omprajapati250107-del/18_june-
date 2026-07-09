a3675000135db2886cd4adf9b2761d1d9d12935b
omprajapati_08 <omprajapati250107@gmail.com>
2026-07-09 11:15:43 +0530

    looping  statment

---
+++ program/pro5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    // step 1 : variable declaration 
+    int num1,num2,ans=0; 
+    
+    // step 2 : variable initlization 
+    num1 = 10;
+    num2 = 20;
+    
+    // logic 
+    ans = num1 + num2;
+    
+    // expected output : 10 + 20 = 30 
+    printf("ans = %d",ans);
+    
+}
